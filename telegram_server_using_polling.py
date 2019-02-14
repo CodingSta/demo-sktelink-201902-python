436c79a01a27f8c89ec26de21de8ca190480dc1b
Chinseok Lee <me@askcompany.kr>
2019-02-14 13:56:20 +0900

    텔레그램 서버 구현

---
+++ telegram_server_using_polling.py
@@ -0,0 +1,43 @@
+import re
+
+from telegram.ext import Updater, Filters
+from telegram.ext import CommandHandler, ConversationHandler, MessageHandler
+
+
+def start(bot, update):
+    chat_id = update.message.chat_id
+    text = update.message.text
+    bot.send_message(chat_id=chat_id, text="I'm a bot, please talk to me!")
+
+
+def echo(bot, update):
+    chat_id = update.message.chat_id
+    text = update.message.text
+
+    # FIXME: 요청에 맞춰, 응답을 해봅시다.
+    response = text  # 앵무새처럼 그대로 따라하기
+
+    bot.send_message(chat_id=chat_id, text=response)
+
+
+def main(token):
+    bot = Updater(token=TOKEN)
+
+    handler = CommandHandler('start', start)
+    bot.dispatcher.add_handler(handler)
+
+    handler = MessageHandler(Filters.text, echo)
+    bot.dispatcher.add_handler(handler)
+
+    bot.start_polling()
+
+    print('running telegram bot ...')
+    bot.idle()
+
+
+if __name__ == '__main__':
+    # TODO: 필요한 라이브러리 설치 : pip install python-telegram-bot
+    # FIXME: 각자의 Token을 적용해주세요.
+    TOKEN = "*********************************************"
+    main(TOKEN)
+
