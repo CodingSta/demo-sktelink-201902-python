3497981f9268b5e9dec4775123b6fbffa57faa24
Chinseok Lee <me@askcompany.kr>
2019-02-15 15:58:23 +0900

    글자수세기/단어수세기/네이버실검 함수 구현

--- telegram_server_using_polling.py
+++ telegram_server_using_polling.py
@@ -7,6 +7,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def 글자수_세기(문자열):
+    return len(문자열)
+
+
+def 단어수_세기(문자열):
+    return len(문자열.split())
+
+
+def 네이버_실검():
+    res = requests.get("http://naver.com")
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    tag_list = soup.select('.PM_CL_realtimeKeyword_rolling .ah_k')
+
+    keyword_list = []
+
+    for tag in tag_list:
+        label = tag.text
+        keyword_list.append(label)
+
+    return keyword_list
+
+
+
 def start(bot, update):
     chat_id = update.message.chat_id
     text = update.message.text
@@ -22,23 +46,16 @@ def echo(bot, update):
             response = '왜?'
         elif text.startswith('글자수세어줘:'):
             문자열 = text[7:]
-            response = '글자수는 {}글자입니다.'.format(len(문자열))
+            글자수 = 글자수_세기(문자열)
+            response = '글자수는 {}글자입니다.'.format(글자수)
         elif text.startswith('단어수세어줘:'):
             문자열 = text[7:]
-            response = '아직 구현 전이야.'  # TODO: 구현
+            단어수 = 단어수_세기(문자열)
+            response = '단어수는 {}글자입니다.'.format(단어수)
         elif text == '네이버실검':
-            res = requests.get("http://naver.com")
-            html = res.text
-            soup = BeautifulSoup(html, 'html.parser')
-            tag_list = soup.select('.PM_CL_realtimeKeyword_rolling .ah_k')
-
-            message_list = []
-
-            for tag in tag_list:
-                label = tag.text
-                message_list.append(label)
-
-            response = "\n".join(message_list)
+            keyword_list = 네이버_실검()
+            response = "\n".join(keyword_list)
+            response = '네이버 실시간 검색어\n\n' + response
         else:
             response = '니가 무슨 말 하는 지 모르겠어. :('
     except Exception as e:
@@ -65,6 +82,6 @@ def main(token):
 if __name__ == '__main__':
     # TODO: 필요한 라이브러리 설치 : pip install python-telegram-bot
     # FIXME: 각자의 Token을 적용해주세요.
-    TOKEN = "*********************************************"
+    import os
+    TOKEN = os.environ.get('TELEGRAM_TOKEN')
     main(TOKEN)
-
